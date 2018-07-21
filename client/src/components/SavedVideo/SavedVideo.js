a016ecf6e6d11bcc6d1481f732bc16d3afdc4dae
Benjamin Shim <benjaminshim@Benjamins-MacBook-Pro.local>
2018-07-20 18:11:25 -0700

    Handle delete video from saved

---
+++ client/src/components/SavedVideo/SavedVideo.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Button, Card, CardBody, CardTitle, CardText } from 'mdbreact';
+
+
+class SavedVideo extends React.Component {
+    render() {
+        return (
+
+            // <div 
+            //     className=" my-2 d-inline-block" 
+            //     style={{margin: "0 auto"}}>
+            //     {/* <iframe 
+            //         className="embed-responsive-item" 
+            //         src={`https://www.youtube.com/embed/${this.props.src}`}
+            //         key={this.props.key}
+            //         onClick={() => this.setMainVideo(this.props.key)}
+            //         ></iframe> */}
+            //     {/* <img 
+            //         src={this.props.thumbnail} 
+            //         key={this.props.src}
+            //         alt=""
+            //         height="125"
+            //         width="225"
+            //         onClick={() => {this.props.deleteVideo(this.props.video)}}/> */}
+
+                   
+            // </div>
+
+            <Card className="w-25 p-3 d-inline-block mx-2 my-2">
+                <CardBody>
+                    <CardTitle>
+                        <iframe 
+                            className="embed-responsive-item" 
+                            src={`https://www.youtube.com/embed/${this.props.src}`}>
+                        </iframe>
+                    </CardTitle>
+                    
+                    <CardText>{this.props.title}</CardText>
+
+                    <Button 
+                    onClick={() => {this.props.deleteVideo(this.props.video)}}>Delete</Button>
+                </CardBody>
+            </Card>
+        )
+    }
+}
+
+export default SavedVideo;
--- client/src/views/Saved/SavedPage.jsx
+++ client/src/views/Saved/SavedPage.jsx
@@ -1,21 +1,37 @@
 import React from 'react';
 
 import SavedItem from '../../components/SavedItem';
+import SavedVideo from '../../components/SavedVideo';
 
 import API from "../../utils/API";
 
 export default class SavedPage extends React.Component {
     state = {
-        results: []
+        results: [],
+        videoResults: []
     }
 
     componentDidMount() {
         this.getSavedArticles();
+        this.getSavedVideos();
     }
 
     getSavedArticles = () => {
         API.getArticles({})
         .then(res => this.setState({results: res.data}))
+        .catch(err => console.log(err))
+    }
+
+    getSavedVideos = () => {
+        API.getVideos({})
+        .then(res => {this.setState(
+            {
+                videoResults: res.data});
+                console.log(res.data);
+
+            }
+            
+        )
         .catch(err => console.log(err))
     }
 
@@ -25,7 +41,20 @@ export default class SavedPage extends React.Component {
         // this.state.results.forEach(i => {
         //     if (id === i._id) {
                 API.deleteArticle(id)
-                .then(res => console.log('deleted'))
+                .then(res => console.log('deleted article'))
+                .catch(err => console.log(err));
+        //     }
+        // })
+
+    }
+
+    handleDeleteVideo = id => {
+        console.log(id._id);
+
+        // this.state.results.forEach(i => {
+        //     if (id === i._id) {
+                API.deleteVideo(id._id)
+                .then(res => console.log('deleted video'))
                 .catch(err => console.log(err));
         //     }
         // })
@@ -35,19 +64,31 @@ export default class SavedPage extends React.Component {
     render() {
         return (
             <div>
+                <h1 className="mt-5">Saved Videos</h1>
+                <div className="container">
+                    {this.state.videoResults.map(i => (
+                        <SavedVideo 
+                            src={i.url}
+                            key={i._id}
+                            title={i.title}
+                            video={i}
+                            deleteVideo={this.handleDeleteVideo}
+                        />
+                ))}
+                </div>
+
                 <h1 className="mt-5">Saved Show Dates</h1>
                 <div className="container">
-
-                {this.state.results.map(i => (
-                    <SavedItem 
-                    title={i.title}
-                    date={i.date}
-                    url={i.url}
-                    key={i._id}
-                    id={i._id}
-                    deleteArticle={this.handleDeleteArticle}
-                    />
-                ))}
+                    {this.state.results.map(i => (
+                        <SavedItem 
+                            title={i.title}
+                            date={i.date}
+                            url={i.url}
+                            key={i._id}
+                            id={i._id}
+                            deleteArticle={this.handleDeleteArticle}
+                        />
+                    ))}
                 </div>
             </div>
         )
